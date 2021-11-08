16c1663540035e29784d51660200707b3dcff941
danebrouwer <49738213+danebrouwer@users.noreply.github.com>
2021-11-08 10:58:00 -0800

    Add files via upload

---
+++ install_detectron2.py
@@ -0,0 +1,6 @@
+import torch
+TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
+CUDA_VERSION = torch.__version__.split("+")[-1]
+print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
+# Install detectron2 that matches the above pytorch version
+# See https://detectron2.readthedocs.io/tutorials/install.html for instructions
